f285865baacf4a513bdce709350110afb2c5a67e
Shan0n73 <agrisyandimaraja@gmail.com>
2025-10-08 17:20:13 +0700

    Percobaan 1

--- jobsheet5/ifCetakKRS01.java
+++ jobsheet5/ifCetakKRS01.java
@@ -2,18 +2,13 @@ import java.util.Scanner;
 
 public class ifCetakKRS01 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    try (Scanner sc = new Scanner(System.in)) {
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (sudah/belum):");
-        String uktLunas = sc.next();
-       
-        if (uktLunas.equalsIgnoreCase("sudah")) {
-        System.out.println("Pembayaran UKT terverivikasi");
-        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        } else {
-        System.out.println("Pembayaran UKT belum terverivikasi");
-        System.out.println("Silakan lunasi UKT terlebih dahulu");
-
-            }
-        }
+        
+        boolean uktlunas = sc.nextBoolean();
+        String hasil = (uktlunas) ? "pembayaran UKT terverifikasi\nsilahkan cetak KRS dan minta tanda tangan dosen DPA" : "pembayaran UKT belum terverifikasi\nsilahkan lunasi pembayaran UKT terlebih dahulu";
+        System.out.println(hasil);
     }
+}
+}

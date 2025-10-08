e2aade7a8713fe16b058f27263c434721cb307eb
Shan0n73 <agrisyandimaraja@gmail.com>
2025-10-08 22:32:43 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS01.java
+++ jobsheet5/ifCetakKRS01.java
@@ -5,7 +5,6 @@ public class ifCetakKRS01 {
     try (Scanner sc = new Scanner(System.in)) {
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (sudah/belum):");
-        
         boolean uktlunas = sc.nextBoolean();
         String hasil = (uktlunas) ? "pembayaran UKT terverifikasi\nsilahkan cetak KRS dan minta tanda tangan dosen DPA" : "pembayaran UKT belum terverifikasi\nsilahkan lunasi pembayaran UKT terlebih dahulu";
         System.out.println(hasil);

b300a783ca9606129dce885452d9787df39354d4
Shan0n73 <agrisyandimaraja@gmail.com>
2025-12-04 09:43:45 +0700

    Percobaan 4

---
+++ jobsheet11/PengunjungCafe01.java
@@ -0,0 +1,18 @@
+package jobsheet11;
+
+public class PengunjungCafe01 {
+
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Dafter Nama Pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+
+    }
+}
